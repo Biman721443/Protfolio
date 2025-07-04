8fed5ba0b82b41eb98faf2b266b63e1ed945b328
Biman721443 <bimangharai721443@gmail.com>
2025-07-04 17:16:40 +0530

    submit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1QUh36H4i5i1GY0QltLRnQB65AU0p_fbP/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -70,7 +70,7 @@ const About = () => {
           >
             <img
               src={profileImage}
-              alt="Tarun Kaushik"
+              alt="Biman Gharai"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
